16dc3449156b873770aef38a036e33c64dae326e
Dakoda Richardson <dakoda@Dakodas-MacBook-Pro.local>
2019-11-06 17:10:35 -0700

    More stubbing for the Importer

--- Importer.py
+++ Importer.py
@@ -6,3 +6,9 @@ class Importer:
     def __init__(self, ingestor, cataloguer):
         self.ingestor = ingestor
         self.cataloguer = cataloguer
+
+    def login(self):
+        pass
+
+    def upload(self):
+        pass
