949f34a2b0a1638f8cbd7fc0d5f9f044b0927259
Dakoda Richardson <dakoda@Dakodas-MBP.app.byu.edu>
2019-11-08 11:59:36 -0700

    More formatting fixes

--- ToolsInterfaces/CataloguerInterface.py
+++ ToolsInterfaces/CataloguerInterface.py
@@ -1,7 +1,9 @@
+from ParameterClasses.AuthToken import AuthToken
+
 class CataloguerInterface:
 
 	def __init__(self):
 		pass
 	
-	def validateSchema(self, schema, datasetID, authToken):
+	def validateSchema(self, schema, datasetID, authToken:AuthToken):
 		return None
