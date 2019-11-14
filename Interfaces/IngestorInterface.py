17a9fc76fa4c31b2cf6d13f66b9373f471fcbabb
benso1 <46542022+benso1@users.noreply.github.com>
2019-11-14 10:48:37 -0700

    Update IngestorInterface.py

--- Interfaces/IngestorInterface.py
+++ Interfaces/IngestorInterface.py
@@ -1,9 +1,10 @@
 import abc
 from ParameterClasses.AuthToken import AuthToken
+from ParameterClasses.DataSetId import DataSetId
+from ParameterClasses.Schema import Schema
 
 class IngestorInterface(abc.ABC):
 
     @abc.abstractmethod
-    def upload(self, file, schema, dataSetID, authToken:AuthToken):
-        pass
-
+    def upload(self, file, schema:Schema, dataSetID:DataSetId, authToken:AuthToken):
+        pass #File is fileInputScheme
