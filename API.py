156ad0b73731e932753561db835ebfbd36c705d1
benso1 <46542022+benso1@users.noreply.github.com>
2019-11-14 10:56:13 -0700

    Create API.py

---
+++ API.py
@@ -0,0 +1,17 @@
+class API:
+  def __init__(self):
+    pass
+  
+  def report(self):
+    pass
+  
+  def validate(self):
+    pass
+  
+  def send(self):
+    pass
+  
+  def upload(self):
+    validate()
+    send()
+    report()
