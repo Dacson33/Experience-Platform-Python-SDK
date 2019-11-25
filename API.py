2fba38b942e94d89a8d51a1a71d8401dce6b271f
Dakoda Richardson <dakoda@Dakodas-MacBook-Pro.local>
2019-11-25 11:52:00 -0700

    Code in the api for getting the access token

--- API.py
+++ API.py
@@ -1,7 +1,16 @@
+import requests
+import json
+
 class API:
 
     def __init__(self):
-        pass
+        with open('config.json') as json_data_file:
+            data = json.load(json_data_file)
+        self.apiKey = data['api_key']
+        self.clientSecret = data['client_secret']
+        self.jwtToken = data['jwt_token']
+        self.imsOrg = data['ims_org']
+        self.access = ''
   
     def report(self):
         pass
@@ -13,6 +22,17 @@ class API:
         pass
   
     def upload(self):
-        self.validate()
-        self.send()
-        self.report()
+        print(self.apiKey)
+        print(self.clientSecret)
+        print(self.jwtToken)
+        files = {
+            'client_id': (None, self.apiKey),
+            'client_secret': (None, self.clientSecret),
+            'jwt_token': (None, self.jwtToken),
+        }
+        testData = requests.post('https://ims-na1.adobelogin.com/ims/exchange/jwt/', files=files)
+        print(testData.json())
+        self.access = testData.json()['access_token']
+
+api = API()
+api.upload()
