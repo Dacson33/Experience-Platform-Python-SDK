1bac38b75529fed70e2b56cc077f3a92feb022a9
Dakoda Richardson <dakoda@Dakodas-MacBook-Pro.local>
2019-12-06 20:46:56 -0700

    Commented the API

--- API.py
+++ API.py
@@ -16,6 +16,7 @@ class API:
         self.apiKey = data['api_key']
         self.clientSecret = data['client_secret']
         self.dID = data['dataID']
+        #Generation of the JWT token
         payload = {
             "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=600),
             "iss": data['ims_org'],
@@ -23,16 +24,13 @@ class API:
             "https://ims-na1.adobelogin.com/s/ent_dataservices_sdk": True,
             "aud": 'https://ims-na1.adobelogin.com/c/' + data['api_key']
         }
-        #print(payload['aud'])
-        #self.jwtToken = data['jwt_token']
         self.jwtToken = jwt.encode(payload, data['secret'], algorithm='RS256').decode('utf-8')
-        #print(self.jwtToken)
         self.imsOrg = data['ims_org']
         self.accessToken = self.access()
-        #self.sandbox = self.sandboxName()
         self.datasetIds = self.dataId()
         self.upload('test128.json', self.dID)
 
+    #Sends a report of the status of the batch to the user
     def report(self, identification):
         headers = {
             'x-gw-ims-org-id': self.imsOrg,
@@ -50,6 +48,7 @@ class API:
     def send(self):
         pass
 
+    #Generates the AuthToken for use with the API
     def access(self):
         files = {
             'client_id': (None, self.apiKey),
@@ -57,12 +56,8 @@ class API:
             'jwt_token': (None, self.jwtToken),
         }
         testData = requests.post('https://ims-na1.adobelogin.com/ims/exchange/jwt/', files=files)
-        #print('Access')
-        #print(testData.json())
         name = testData.json()['access_token']
-        #print(name)
         expiration = testData.json()['expires_in']
-        #print(expiration)
         authorization = AuthToken(name, expiration)
         return authorization
 
@@ -84,29 +79,28 @@ class API:
         #return response.json()['name']
         return ""
 
+    #Gets the datasetIDs tied to the user
     def dataId(self):
         headers = {
             'Authorization': 'Bearer ' + self.accessToken.getToken(),
             'x-api-key': self.apiKey,
             'x-gw-ims-org-id': self.imsOrg,
-            #'x-sandbox-name': self.sandbox,
         }
         params = (
             ('limit', '5'),
             ('properties', 'name'),
         )
         response = requests.get('https://platform.adobe.io/data/foundation/catalog/dataSets', headers=headers, params=params)
-        #print('DataSetID')
-        #print('  ' + response.json())
         ids = []
         #In order to get a specific datasetID what we could do is iterate through the response and create multiple datasetID objects that way since we can access the key by index since response in an unordered dict
         for id in response.json():
             datasetID = DataSetId(id)
             ids.append(datasetID)
-        #datasetID = DataSetId(response.json())
         return ids
 
+    #Uploads the file to Experience Platform
     def upload(self, fileName, datasetId):
+        #Creates the batch
         headers = {
             'Content-Type': 'application/json',
             'x-gw-ims-org-id': self.imsOrg,
@@ -117,17 +111,18 @@ class API:
         response = requests.post('https://platform.adobe.io/data/foundation/import/batches', headers=headers, data=data)
         print('Create batch status: ' + response.json()['status'])
         batchId = response.json()['id']
+        #Uploads the file
         headers = {
             'Content-Type': 'application/octet-stream',
             'x-gw-ims-org-id': self.imsOrg,
             'Authorization': 'Bearer ' + self.accessToken.getToken(),
             'x-api-key': self.apiKey
         }
-
         print('File upload of ' + fileName + ' in progress')
         data = open('Tests/' + fileName, 'rb').read()
         response = requests.put('https://platform.adobe.io/data/foundation/import/batches/' + batchId + '/datasets/' + datasetId + '/files/' + fileName, headers=headers, data=data)
         print(response)
+        #Signals the completion of the batch
         headers = {
             'x-gw-ims-org-id': self.imsOrg,
             'Authorization': 'Bearer ' + self.accessToken.getToken(),
