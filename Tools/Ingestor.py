3dcafbad4a771e4fb194a1c484c556186c00bc19
Dakoda Richardson <dakoda@Dakodas-MBP.app.byu.edu>
2020-01-22 11:54:39 -0700

    Got the large file upload working

--- Tools/Ingestor.py
+++ Tools/Ingestor.py
@@ -4,7 +4,8 @@ from ParameterClasses.AuthToken import AuthToken
 from ParameterClasses.DataSetId import DataSetId
 import requests
 import os
-import bitmath
+from bitmath import MiB
+from fsplit.filesplit import FileSplit
 
 class Ingestor(IngestorInterface):
 
@@ -14,7 +15,7 @@ class Ingestor(IngestorInterface):
 #    def upload(self, file, schema:Schema, dataSetID:DataSetId, authToken:AuthToken):
 #        pass
 
-    def upload(self, fileName, datasetId:DataSetId, imsOrg, accessToken:AuthToken, apiKey, cataloguer):
+    def upload(self, fileName, datasetId, imsOrg, accessToken:AuthToken, apiKey, cataloguer):
         #Creates the batch
         headers = {
             'Content-Type': 'application/json',
@@ -35,7 +36,7 @@ class Ingestor(IngestorInterface):
         }
         print('File upload of ' + fileName + ' in progress')
         #data = open('Tests/' + fileName, 'rb').read()
-        data = open(fileName, 'rb').read()
+        data = open('Tests/' + fileName, 'rb').read()
         response = requests.put('https://platform.adobe.io/data/foundation/import/batches/' + batchId + '/datasets/' + datasetId + '/files/' + fileName, headers=headers, data=data)
         print(response)
         #Signals the completion of the batch
@@ -52,7 +53,7 @@ class Ingestor(IngestorInterface):
         print(response)
         cataloguer.report(batchId, imsOrg, accessToken, apiKey)
 
-    def uploadLarge(self, fileName, dataSetId:DataSetId, imsOrg, accessToken:AuthToken, apiKey, cataloguer):
+    def uploadLarge(self, fileName, dataSetId, imsOrg, accessToken:AuthToken, apiKey, cataloguer):
         headers = {
             'Content-Type': 'application/json',
             'x-gw-ims-org-id': imsOrg,
@@ -63,13 +64,33 @@ class Ingestor(IngestorInterface):
         response = requests.post('https://platform.adobe.io/data/foundation/import/batches', headers=headers, data=data)
         print('Create batch status: ' + response.json()['status'])
         batchId = response.json()['id']
+        fs = FileSplit(file='Tests/' + fileName, splitsize=256000000, output_dir='Splits/')
+        fs.split(include_header=True)
+        for entry in os.scandir('Splits/'):
+            print(entry.path)
+            headers = {
+                'Content-Type': 'application/octet-stream',
+                'x-gw-ims-org-id': imsOrg,
+                'Authorization': 'Bearer ' + accessToken.getToken(),
+                'x-api-key': apiKey
+            }
+            print('File upload of ' + os.path.basename(entry.path) + ' in progress')
+            data = open(entry.path, 'rb').read()
+            response = requests.put(
+                'https://platform.adobe.io/data/foundation/import/batches/' + batchId + '/datasets/' + dataSetId + '/files/' + os.path.basename(entry.path),
+                headers=headers, data=data)
+            print(response)
+
         headers = {
             'x-gw-ims-org-id': imsOrg,
             'Authorization': 'Bearer ' + accessToken.getToken(),
-            'x-api-key': apiKey,
+            'x-api-key': apiKey
         }
         params = (
-            ('action', 'initialize'),
+            ('action', 'COMPLETE'),
         )
-        response = requests.post('https://platform.adobe.io/data/foundation/import/batches/' + batchId + '/datasets/' + dataSetId + '/files/part1=a/part2=b/' + fileName, headers=headers, params=params)
+        print('Signal Completion: ')
+        response = requests.post('https://platform.adobe.io/data/foundation/import/batches/' + batchId,
+                                 headers=headers, params=params)
         print(response)
+        cataloguer.report(batchId, imsOrg, accessToken, apiKey)
